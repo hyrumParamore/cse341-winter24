15dfdceb2bb4c86d57476812d9bc442910e9bea5
Hyrum Paramore <par17057@byui.edu>
2024-01-27 11:46:52 -0700

    Finished Week 3

--- routes/contacts.js
+++ routes/contacts.js
@@ -7,6 +7,7 @@ const contactsController = require('../controllers/contacts');
 
 // Week 2 - GET Route to retrieve contacts from database.
 router.get('/', contactsController.getAll);
+// Week 2
 router.get('/:id', contactsController.getSingle);
 
 // Middleware to check if input fields are filled in or not.
