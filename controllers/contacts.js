9d75ad8615d21ddb11d664435ee6a28ec59bf0d5
Hyrum Paramore <par17057@byui.edu>
2024-02-01 14:27:04 -0700

    created some try catch blocks to make the server not crash XD

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -4,77 +4,96 @@ const ObjectId = require('mongodb').ObjectId;
 
 // Week 2 - Get All Contacts
 const getAll = async (req, res) => {
-  const result = await mongodb.getDb().db().collection('contacts').find();
-  result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists);
-  });
+  try {
+    const result = await mongodb.getDb().db().collection('contacts').find();
+    result.toArray().then((lists) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(lists);
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+  
 };
 
 
 // Week 2 - Get Single Contact From ID
 const getSingle = async (req, res) => {
-  const userId = new ObjectId(req.params.id);
-  const result = await mongodb
-    .getDb()
-    .db()
-    .collection('contacts')
-    .find({ _id: userId });
-  result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists[0]);
-  });
+  try {
+    const userId = new ObjectId(req.params.id);
+    const result = await mongodb
+      .getDb()
+      .db()
+      .collection('contacts')
+      .find({ _id: userId });
+    result.toArray().then((lists) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(lists[0]);
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+  
 };
 
 
 // Week 3 - Create Contact
 const createContact = async (req, res ) => {  
-  const contact = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    favoriteColor: req.body.favoriteColor,
-    birthday: req.body.birthday
-  };
+  try {
+    const contact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday
+    };
 
-  const response = await mongodb.getDb().db().collection('contacts').insertOne(contact);
-  if (response.acknowledged) {
-    res.status(201).json(response);
-  } else {
-    res.status(500).json(response.error || 'Some error occurred while creating the contact.');
+    const response = await mongodb.getDb().db().collection('contacts').insertOne(contact);
+    if (response.acknowledged) {
+      res.status(201).json(response);
+    } else {
+      res.status(500).json(response.error || 'Some error occurred while creating the contact.');
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
 };
 
 
 // Week 3 - Updates Contact based off contact ID
 const updateContact = async (req, res) => {
-  const userId = new ObjectId(req.params.id);
 
-  const contact = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    favoriteColor: req.body.favoriteColor,
-    birthday: req.body.birthday
-  };
+  try {
+    const userId = new ObjectId(req.params.id);
 
-  const response = await mongodb.getDb().db().collection('contacts').replaceOne({ _id: userId}, contact);
+    const contact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday
+    };
+
+    const response = await mongodb.getDb().db().collection('contacts').replaceOne({ _id: userId}, contact);
 
 
-  console.log(response);
-  if (response.modifiedCount > 0) {
-    res.status(204).send();
-  } else {
-    res.status(500).json(response.error || 'Some error occurred while updating the contact.');
+    console.log(response);
+    if (response.modifiedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(500).json(response.error || 'Some error occurred while updating the contact.');
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
 };
 
 
 // Week 3 - Delete Contact based off contact ID
 const deleteContact = async (req, res) => {
-  const userId = new ObjectId(req.params.id);
-
   try {
+    const userId = new ObjectId(req.params.id);
+
     const response = await mongodb.getDb().db().collection('contacts').deleteOne({ _id: userId });
 
     if (response.deletedCount > 0) {
@@ -82,9 +101,8 @@ const deleteContact = async (req, res) => {
     } else {
       res.status(404).json({ error: 'Contact not found' });
     }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
 };
 
