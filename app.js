e0d9c4d5456b8ac0396ddd85281074df90fdfa00
Hyrum Paramore <par17057@byui.edu>
2024-02-01 13:40:01 -0700

    Fixed an error that was popping up when trying to connect to the React app for Extra Credit

--- app.js
+++ app.js
@@ -2,18 +2,26 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
 
-// Swagger API Documentation
-// const swaggerUi = require('swagger-ui-express');
-// const swaggerDocument = require('./swagger.json');
-
 const port = process.env.PORT || 8080;
 const app = express();
 
 app
-  // .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
   .use(bodyParser.json())
   .use((req, res, next) => {
+    // Allow requests from any origin
     res.setHeader('Access-Control-Allow-Origin', '*');
+    // Specify the allowed HTTP methods
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    // Specify the allowed headers
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    // Allow credentials (if needed)
+    // res.setHeader('Access-Control-Allow-Credentials', 'true');
+    
+    // Handle preflight requests
+    if (req.method === 'OPTIONS') {
+      return res.status(200).end();
+    }
+
     next();
   })
   .use('/', require('./routes'));
