65800344c2599b5c4b7ba11da3f8fb361a309868
Hyrum Paramore <par17057@byui.edu>
2024-02-06 18:04:55 -0700

    another try to fix the render/api-doc

--- app.js
+++ app.js
@@ -6,21 +6,9 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 app
-  // .use(bodyParser.json())
-  // .use((req, res, next) => {
-  //   res.setHeader('Access-Control-Allow-Origin', '*');
-  //   next();
-  // })
-  // .use('/', require('./routes'));
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
-    );
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
   })
   .use('/', require('./routes'));
@@ -33,3 +21,39 @@ mongodb.initDb((err) => {
     console.log(`Connected to DB and listening on ${port}`);
   }
 });
+
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const mongodb = require('./db/connect');
+
+// const port = process.env.PORT || 8080;
+// const app = express();
+
+// app
+//   // .use(bodyParser.json())
+//   // .use((req, res, next) => {
+//   //   res.setHeader('Access-Control-Allow-Origin', '*');
+//   //   next();
+//   // })
+//   // .use('/', require('./routes'));
+//   .use(bodyParser.json())
+//   .use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader(
+//       'Access-Control-Allow-Headers',
+//       'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+//     );
+//     res.setHeader('Content-Type', 'application/json');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//     next();
+//   })
+//   .use('/', require('./routes'));
+
+// mongodb.initDb((err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     app.listen(port);
+//     console.log(`Connected to DB and listening on ${port}`);
+//   }
+// });
