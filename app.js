df3a1850946bba8a38b84c311bfb8e87936529f0
Hyrum Paramore <par17057@byui.edu>
2024-02-06 18:11:04 -0700

    another change

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
     next();
   })
   .use('/', require('./routes'));
